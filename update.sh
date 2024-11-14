7c7086e7b4817c7840fe2b9e52f517eccd0a5489
autoios <autoios@google.com>
2024-11-14 22:02:10 +0700

    123

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+dpkg-scanpackages -m ./ > Packages
 bzip2 -c9k ./Packages > ./Packages.bz2
 gzip -c9k ./Packages > ./Packages.gz
 printf "Architectures: iphoneos-arm iphoneos-arm64 iphoneos-arm64e\nCodename: autotouch-repo\nComponents: main\nDate: $(date -Ru)\nOrigin: Autoios AutoTouch\nLabel: Autoios AutoTouch\nSuite: stable\nComponents: main\nDescription: Autoios AutoTouch\nVersion: 1.0\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n "$(cat ./Packages.gz | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.gz)" Packages.gz\n" >Release;
